0d0fd9ce7b9a9eb316a7217125d68d2df1b20b0c
fho-wtag <faruk.hossain@welltravel.com>
2016-07-27 18:00:11 +0600

    Test if blizzard client is callable or not

--- lib/ret-urn-er.rb
+++ lib/ret-urn-er.rb
@@ -1,7 +1,10 @@
-require "ret-urn-er/version"
+require 'ret-urn-er/version'
 
 module RetURNer
   def self.hello(str)
     puts str
+    if Blizzard::Client::Customer.respond_to?(:find)
+      puts 'Blizzard is cllable'
+    end
   end
 end
