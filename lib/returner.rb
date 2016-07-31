246465b42649dfa21fd6a010ba84f85a2cf000ed
fho-wtag <faruk.hossain@welltravel.com>
2016-07-31 15:59:05 +0600

    Add support for transaction and voucher

--- lib/returner.rb
+++ lib/returner.rb
@@ -3,10 +3,15 @@ require 'blizzard-client'
 
 class RetURNer
   LOOKUPS = {
-    customer: ->(id) { Blizzard::Client::Customer.find(id: id) }
+    customer: ->(id) { Blizzard::Client::Customer.find(id: id) },
+    order: ->(id, company) { Blizzard::Client::Transaction.by_customer(id: id, company: company) },
+    voucher: ->(id) { Blizzard::Client::Voucher.by_customer(id: id) }
   }.freeze
+
   TYPE_MAPPING = {
     customer: 'urn:fadendaten:customer',
+    order: 'urn:fadendaten:webshop:order',
+    voucher: 'urn:fadendaten:voucher'
   }.freeze
 
   attr_reader :urn
@@ -23,20 +28,29 @@ class RetURNer
     @type ||= TYPE_MAPPING.invert[splitted_urn[:type]]
   end
 
+  def other_id
+    splitted_urn_hash = Hash[ splitted_urn.names.zip( splitted_urn.captures ) ]
+    @other_id ||= splitted_urn_hash['other_id']
+  end
+
   def fetch
+    return LOOKUPS[type].call(id, other_id) if other_id
     LOOKUPS[type].call(id)
   end
 
   def splitted_urn
-    @splitted_urn ||= urn.match(/(?<type>.+):(?<id>\d+)\z/)
+    @splitted_urn ||= urn.match(/(?<type>.+):(?<id>\d+)\z/) ||
+                      urn.match(/(?<type>.+):(?<id>\d+):(?<other_id>.+)\z/)
   end
 
   def to_s
     urn
   end
 
-  def self.for(type, id)
-    urn = '%s:%d'.format(TYPE_MAPPING[type], id)
+  def self.for(type: nil, id: nil, other_id: nil)
+    urn = "#{TYPE_MAPPING[type]}"
+    urn += ":#{id}" if id
+    urn += ":#{other_id}" if other_id
     new(urn)
   end
 end
